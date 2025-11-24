dcf4c1aaa0808550bb3c635142099a642c60acbd
Saif <mdsaif123654@gmail.com>
2025-11-24 13:25:38 +0530

    Added render.yaml and build.sh

--- symptoms_pro/settings.py
+++ symptoms_pro/settings.py
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'symptoms_pro.urls'
@@ -115,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 import os 
 STATIC_DIRS=[os.path.join(BASE_DIR,'static')]
